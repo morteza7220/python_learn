c9ee92d7b1eeee28c4ecbc27f6e09a20ed32a32f
morteza_shabani_pour <hafez7xp@gmail.com>
2023-11-09 12:08:26 +0100

    two

---
+++ one.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import re
+import subprocess
+from subprocess import PIPE, Popen
+arr=[]
+def ping(a):
+    process = Popen(['ping', '-c','1','time.ir'],stdout=PIPE, stderr=PIPE)
+    out, err = process.communicate()
+    x=re.search("time=(?P<time>[0-9]{1,5})" , str(out))
+
+    arr.append(x.groupdict()['time'])
+    
+    if len(arr) > 10:
+        arr.pop(0)
+    plt.cla()
+    plt.plot(arr)
+    plt.xlim(-1,11) 
+    plt.ylim(0,30)
+    plt.ylabel("mori is best")
+    plt.title("and the best")
+    
+ani=animation.FuncAnimation(plt.gcf(), ping, interval=1000)
+
+
+
+
+
+plt.show()
